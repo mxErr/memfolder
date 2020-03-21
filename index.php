77b92495db2a7dcc5e21c680b2334dee5d8d0ecc
mxErr <wwx@bk.ru>
2020-03-21 12:41:15 +0300

    Add menu

--- index.php
+++ index.php
@@ -7,8 +7,23 @@
 </head>
 <body>
     <h1>Hello World</h1>
-<?php $categories = ['Главная', 'Мемы', 'Гифки', 'Приколы']
+<?php 
+$categories = ['Главная', 'Мемы', 'Гифки', 'Приколы'];
+?>
 
+<div class="navigation__item">
+    <h3 class="navigation__title">Категории</h3>
+    <nav class="navigation__links">
+    <?php $index = 0;
+    $num = count($categories);
+    while($index < $num) {
+        $cat = $categories[$index];
+        print('<br><a href="#">' . $cat . '</a>');
+        $index = $index + 1;
+    }
+?>
+</nav>
+</div>
 </body>
             </html>
             
